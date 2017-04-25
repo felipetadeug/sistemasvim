8a52fadc79f4932efef23a694ce73ec1e86370a1
caue.ghetti <caue.ghetti@CAS511781W0732.senacsp.edu.br>
2017-04-25 19:57:08 -0300

    Implementação do projeto e seus métodos.

---
+++ src/main/java/br/com/svim/servlet/FilialControl.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.svim.servlet;
+
+import br.com.svim.dao.FilialDao;
+import br.com.svim.model.Filial;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Cauê Ghetti
+ */
+public class FilialControl extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        Filial filial = new Filial();
+
+        filial.setNome_filial(request.getParameter("FilialNome"));
+        filial.setRua(request.getParameter("FilialRua"));
+        filial.setBairro(request.getParameter("FilialBairro"));
+        filial.setCidade(request.getParameter("FilialCidade"));
+        filial.setUf(request.getParameter("FilialUF"));
+        filial.setCep(request.getParameter("FilialCEP"));
+        filial.setNumero(12);
+
+        FilialDao daoCadatro = new FilialDao();
+        daoCadatro.cadastrarFilial(filial);
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(FilialControl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(FilialControl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
