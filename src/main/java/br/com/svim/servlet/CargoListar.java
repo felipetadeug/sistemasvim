7d3cc38949a8f539d687ed933a91b4bea4ab2b10
Cauê Ghetti <Cau Ghetti@CDFSP3076OP>
2017-05-14 23:24:51 -0300

    Servlets Listar e Cadastrar Nova Filial já feitas e Funcionando Perfeitamente

--- src/main/java/br/com/svim/servlet/CargoListar.java
+++ src/main/java/br/com/svim/servlet/CargoListar.java
@@ -9,6 +9,8 @@ import br.com.svim.controller.CargoController;
 import br.com.svim.model.Cargo;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,9 +18,9 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author caue.ghetti
+ * @author Cauê Ghetti
  */
-public class CargoCadastrar extends HttpServlet {
+public class CargoListar extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -32,52 +34,62 @@ public class CargoCadastrar extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        Cargo cargo = new Cargo();
+        /*
+        
+        Colocar dentro o JSP de Alterar Cargo estão função lista os Cargos 
+        
+        <c:forEach var="cargo" items="${ListCargo}">
+            <tr>
+                <td>${cargo.getCargo()}</td>
+                <td>${cargo.getHierarquia()}</td>
+            </tr>
+        </c:forEach>
+         */
+        List<Cargo> cargoList = new ArrayList<>();
+        CargoController control = new CargoController();
 
-        cargo.setCargo(request.getParameter(""));
-        cargo.setHierarquia(Integer.parseInt(request.getParameter("")));
+        Cargo cargo = new Cargo();
 
         try {
 
-            CargoController cargoCon = new CargoController();
-            cargoCon.cadastrar(cargo);
-            
+            cargoList = control.obter();
+            request.setAttribute("ListCargo", cargoList);
             request.getRequestDispatcher("").forward(request, response);
 
         } catch (Exception e) {
-
             System.err.println("ERROR-----> " + e);
 
         }
 
     }
-    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        Cargo cargo = new Cargo();
-
-        cargo.setCargo(request.getParameter(""));
-        cargo.setHierarquia(Integer.parseInt(request.getParameter("")));
-
-        try {
-
-            CargoController cargoCon = new CargoController();
-            cargoCon.cadastrar(cargo);
-
-        } catch (Exception e) {
-
-            System.err.println("ERROR-----> " + e);
-
-        }
-
+        processRequest(request, response);
     }
 
     /**
@@ -85,4 +97,9 @@ public class CargoCadastrar extends HttpServlet {
      *
      * @return a String containing servlet description
      */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
