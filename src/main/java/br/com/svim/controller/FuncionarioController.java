9a13741c0f2da6115639d011f53d08aa9f6fe456
luis.hlsousa <luis.hlsousa@CAS511781W0732.senacsp.edu.br>
2017-05-29 21:50:03 -0300

    Validacao de filial, cargo e cpf de funcionario

--- src/main/java/br/com/svim/controller/FuncionarioController.java
+++ src/main/java/br/com/svim/controller/FuncionarioController.java
@@ -18,6 +18,7 @@ public class FuncionarioController {
 
     public static void cadastrar(Funcionario funcionario) throws Exception {
         try {
+            validar(funcionario);
             DaoFuncionario.cadastrar(funcionario);
         } catch (Exception e) {
             throw e;
@@ -26,6 +27,7 @@ public class FuncionarioController {
 
     public static void alterar(Funcionario funcionario) throws Exception {
         try {
+            validar(funcionario);
             DaoFuncionario.alterar(funcionario);
         } catch (Exception e) {
             throw e;
@@ -73,66 +75,27 @@ public class FuncionarioController {
             if (funcionario.getNome() == null || funcionario.getNome().trim().isEmpty()) {
                 throw new Exception("Campo Nome vazio");
             }
-            if (!isCPF(funcionario.getCpf())) {
-                throw new Exception("CPF inválido");
+            validarCPF(funcionario.getCpf());
+
+            if (!(funcionario.getFilial().getIdFilial() <= 0)) {
+                throw new Exception("Filial inválida");
             }
+            if (!(funcionario.getCargo().getIdCargo() <= 0)) {
+                throw new Exception("Cargo inválido");
+            }
+
         } catch (Exception e) {
             throw e;
         }
     }
 
-    private static boolean isCPF(String CPF) {
-
-        if (CPF.equals("00000000000") || CPF.equals("11111111111")
-                || CPF.equals("22222222222") || CPF.equals("33333333333")
-                || CPF.equals("44444444444") || CPF.equals("55555555555")
-                || CPF.equals("66666666666") || CPF.equals("77777777777")
-                || CPF.equals("88888888888") || CPF.equals("99999999999")
-                || (CPF.length() != 11)) {
-            return (false);
-        }
-
-        char dig10, dig11;
-        int sm, i, r, num, peso;
-
+    private static void validarCPF(String CPF) throws Exception {
         try {
-            sm = 0;
-            peso = 10;
-            for (i = 0; i < 9; i++) {
-                num = (int) (CPF.charAt(i) - 48);
-                sm = sm + (num * peso);
-                peso = peso - 1;
+            if (!(CPF.length() == 11)) {
+                throw new Exception("CPF inválido");
             }
-
-            r = 11 - (sm % 11);
-            if ((r == 10) || (r == 11)) {
-                dig10 = '0';
-            } else {
-                dig10 = (char) (r + 48);
-            }
-
-            sm = 0;
-            peso = 11;
-            for (i = 0; i < 10; i++) {
-                num = (int) (CPF.charAt(i) - 48);
-                sm = sm + (num * peso);
-                peso = peso - 1;
-            }
-
-            r = 11 - (sm % 11);
-            if ((r == 10) || (r == 11)) {
-                dig11 = '0';
-            } else {
-                dig11 = (char) (r + 48);
-            }
-
-            if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10))) {
-                return (true);
-            } else {
-                return (false);
-            }
-        } catch (InputMismatchException erro) {
-            return (false);
+        } catch (Exception e) {
+            throw e;
         }
     }
 }
