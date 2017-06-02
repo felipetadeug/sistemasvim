33e02769db417500af0d881bd90b2b8dc7f9e377
Iago <Iago@PC-Guimaraes>
2017-06-02 00:02:52 -0300

    inserir dados principais na ddl.sql

--- Documentação/DDL.sql
+++ Documentação/DDL.sql
@@ -1189,3 +1189,98 @@ DELIMITER ;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2017-04-25 20:47:08
+
+
+
+
+
+
+
+
+
+
+
+
+-- ####################################################
+-- INSERIR OS DADOS
+-- ####################################################
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: autopostosvim
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `cargo`
+--
+
+LOCK TABLES `cargo` WRITE;
+/*!40000 ALTER TABLE `cargo` DISABLE KEYS */;
+INSERT INTO `cargo` VALUES (1,'Frentista',1,1),(2,'Gerente',2,1),(3,'Admin',3,1);
+/*!40000 ALTER TABLE `cargo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `filial`
+--
+
+LOCK TABLES `filial` WRITE;
+/*!40000 ALTER TABLE `filial` DISABLE KEYS */;
+INSERT INTO `filial` VALUES (1,'Filial de Campinas','Rua dos Milhos',150,'Sucrilhos','Campinas','SP','15476000',1),(2,'Filial de Sao Paulo','Rua dos Falecidos',145,'Agiota','Sao Paulo','SP','34687000',1),(3,'Filial do Rio','Morro do Dende',147,'Dificil de Invadir','Rio de Janeiro','RJ','97699335',1),(4,'Filial de Minas','Rua do Queijo',360,'Provolone','Belo Horizonte','MG','24547660',1);
+/*!40000 ALTER TABLE `filial` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `funcionario`
+--
+
+LOCK TABLES `funcionario` WRITE;
+/*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
+INSERT INTO `funcionario` VALUES (1,'Administrador','11111111111','2000-01-01 00:00:00','2000-01-01 00:00:00',3,1,'admin',1);
+/*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- Dumping data for table `produto`
+--
+
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+INSERT INTO `produto` VALUES (1,'Gasolina',1,3.40,1,1,1),(2,'Alcool',1,2.99,1,1,1),(3,'Gasolina Aditivada',1,3.70,1,1,1),(4,'Diesel',1,3.60,1,1,1),(5,'Gasolina Premium',1,4.00,1,1,1),(6,'Troca de Oleo',2,25.00,1,0,0),(7,'Lavagem Simples',2,35.00,1,0,0),(8,'Lavagem Completa',2,60.00,1,0,0),(9,'Oleo',3,30.00,1,0,1),(10,'Limpador de Parabrisa',3,40.00,1,0,1),(11,'Pretinho',3,25.00,1,0,1),(12,'Limpa Plastico',3,18.00,1,0,1);
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `tipo_produto`
+--
+
+LOCK TABLES `tipo_produto` WRITE;
+/*!40000 ALTER TABLE `tipo_produto` DISABLE KEYS */;
+INSERT INTO `tipo_produto` VALUES (1,'Combustivel',1),(2,'Prestacao de Servico',1),(3,'Produto',1);
+/*!40000 ALTER TABLE `tipo_produto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-06-01 23:59:11
