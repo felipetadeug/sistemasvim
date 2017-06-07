8c8909616c63fcaca90fc7b90bbb7746a9408c41
caue.ghetti <caue.ghetti@CAS511781W0732.senacsp.edu.br>
2017-06-07 19:30:48 -0300

    ajuste table click tela produto

--- src/main/webapp/produto.js
+++ src/main/webapp/produto.js
@@ -1,3 +1,6 @@
+// instancia um objeto vazio para usar como produto (produto selecionada)
+var produto = {};
+
 window.addEventListener('load', function () {
 
     // se houver erro na tela, mostra o modal
@@ -7,25 +10,6 @@ window.addEventListener('load', function () {
 
     // tabela com paginacao e busca
     $('table').DataTable();
-
-    // instancia um objeto vazio para usar como produto (produto selecionada)
-    var produto = {};
-
-    // ao clicar em uma linha da tabela
-    $('.table tr').click(function () {
-        // adiciona a classe selected na linha e remove da antiga
-        $(this).addClass('selected').siblings().removeClass('selected');
-
-        // obtem os dados contidos em cada coluna e preenche o objeto
-        produto.id = $(this).find('td:nth-child(1)').html();
-        produto.produto = $(this).find('td:nth-child(2)').html();
-        produto.id_tipo_produto = $(this).find('td:nth-child(3)').html();
-        produto.tipo_produto = $(this).find('td:nth-child(4)').html();
-        produto.preco = $(this).find('td:nth-child(5)').html();
-        produto.combustivel = $(this).find('td:nth-child(6)').html() == 'true';
-        produto.estocavel = $(this).find('td:nth-child(7)').html() == 'true';
-
-    });
 
     // ao clicar no botao de EDITAR (abrir modal)
     $('button[data-target="#editarProduto"]').click(function () {
@@ -38,9 +22,9 @@ window.addEventListener('load', function () {
         $('#editarProduto [name=id]').val(produto.id);
         $('#editarProduto [name=produto]').val(produto.produto);
         $('#editarProduto [name=preco]').val(produto.preco);
-        $('#editarProduto [name=tipo_produto]').val(produto.id_tipo_produto);            
-        $('#editarProduto [name=combustivel]').prop('checked',produto.combustivel);
-        $('#editarProduto [name=estocavel]').prop('checked',produto.estocavel);
+        $('#editarProduto [name=tipo_produto]').val(produto.id_tipo_produto);
+        $('#editarProduto [name=combustivel]').prop('checked', produto.combustivel);
+        $('#editarProduto [name=estocavel]').prop('checked', produto.estocavel);
     });
 
     // ao clicar no botao de DELETAR (abrir modal)
@@ -53,5 +37,22 @@ window.addEventListener('load', function () {
         // envia os dados pros campos da modal
         $('#deletarProduto [name=id]').val(produto.id);
     });
+
+});
+
+// ao clicar em uma linha da tabela
+$(document).on('click', ".table tr", function () {
+
+    // adiciona a classe selected na linha e remove da antiga
+    $(this).addClass('selected').siblings().removeClass('selected');
+
+    // obtem os dados contidos em cada coluna e preenche o objeto
+    produto.id = $(this).find('td:nth-child(1)').html();
+    produto.produto = $(this).find('td:nth-child(2)').html();
+    produto.id_tipo_produto = $(this).find('td:nth-child(3)').html();
+    produto.tipo_produto = $(this).find('td:nth-child(4)').html();
+    produto.preco = $(this).find('td:nth-child(5)').html();
+    produto.combustivel = $(this).find('td:nth-child(6)').html() == 'true';
+    produto.estocavel = $(this).find('td:nth-child(7)').html() == 'true';
 
 });
